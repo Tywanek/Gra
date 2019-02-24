de869f4a086cdf67c67e5f619b1921eae5b1d6c3
Radek <radoslaw.tywanek@gmail.com>
2019-02-24 11:13:27 +0100

    init commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int x = getRandomInt();
+        sprawdz(x);
+    }
+
+    public static void sprawdz(int x) {
+        int liczba = getUserValue();
+
+        if (x > liczba) {
+            System.out.println("X jest wieksza");
+            sprawdz(x);
+        } else if (x < liczba) {
+            System.out.println("X jest mniejsza");
+            sprawdz(x);
+        } else {
+            System.out.println("Sukces");
+        }
+    }
+
+    public static int getRandomInt() {
+        Random random = new Random();
+        return random.nextInt(100);
+    }
+
+    public static int getUserValue() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbe: ");
+        return scanner.nextInt();
+    }
+
+
+}
